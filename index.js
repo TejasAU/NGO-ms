a9195ab9b1bd906a4a3a7f7a6c550e49c1b0acaf
TejasAU <tejas.ulabhaje@gmail.com>
2023-10-12 11:18:19 +0530

    added script to start server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const body = require('body-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql2');
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// const db = mysql.createpool({
+//     host:"localhost",
+//     user:"root",
+//     password:"root",
+//     database:""
+// })
+
+app.get("/" , (req , res) => {
+    res.send("Hello Express");
+})
+
+app.listen(5000, () => {
+    console.log('hello world');
+})
