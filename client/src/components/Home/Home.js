6812b8c85c462db6b849babdc5700049d70e602e
TejasAU <tejas0418@gmail.com>
2023-10-28 18:54:59 +0530

    created table

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,11 +2,60 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css"
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Home = () => {
+    const [data, setData ] = useState([]);
+
+    const loadData = async () => {
+        const response = await axios.get("http://localhost:5000/api/get");
+        setData(response.data);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, []);
     return(
-        <div>
-            <h2>Home</h2>
+        <div style={{marginTop: "150 px"}}>
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th style={{textAlign: "center"}}>No.</th>
+                        <th style={{textAlign: "center"}}>Name</th>
+                        <th style={{textAlign: "center"}}>Email</th>
+                        <th style={{textAlign: "center"}}>Contact</th>
+                        <th style={{textAlign: "center"}}>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item , index) => {
+                        return(
+                            <tr key={item.id}>
+                                <th scope="row">{index+1}</th>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.contact}</td>
+                                <td>
+                                    <Link to={`/update/${item.id}`}>
+                                    <button className="btn btn-edit">
+                                        Edit
+                                    </button>
+                                    </Link>
+                                    <button className="btn btn-delete">
+                                        Delete
+                                    </button>
+                                    <Link to={`/view/${item.id}`}>
+                                    <button className="btn btn-view">
+                                        View
+                                    </button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     )
 }
