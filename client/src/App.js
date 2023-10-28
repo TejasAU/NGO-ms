7d6e5bc56ab99d0f7485481f982868a1116484b6
TejasAU <tejas0418@gmail.com>
2023-10-28 18:06:01 +0530

    client side directories added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello client</h1>
+    </div>
+  );
+}
+
+export default App;
