ccb1b34402a02c2ac961195b7546332d06724044
TejasAU <tejas0418@gmail.com>
2023-10-31 11:02:24 +0530

    routing

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <table className="styled-table">
     <thead>
     <tr>
-        <th>components</th>
+        <th>Components</th>
         <th>Action</th>
     </tr>
     </thead>
@@ -13,18 +13,112 @@ function Home() {
         <td>Partners</td>
         <td><Link to={`/partners`}>
                                     <button className="btn btn-edit">
-                                        Partners
+                                        View
                                     </button>
-                                    </Link></td>
+                                    </Link>
+                                    </td>
     </tr>
     <tr>
         <td>Donors</td>
-        <td>Female</td>
+        <td><Link to={`/donor`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    <tr>
+        <td>Staff</td>
+        <td><Link to={`/staff`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    <tr>
+        <td>Event</td>
+        <td><Link to={`/event`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    <tr>
+        <td>Work</td>
+        <td><Link to={`/work`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    <tr>
+        <td>Supervisors</td>
+        <td><Link to={`/supervisor`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    <tr>
+        <td>Awareness Sessions</td>
+        <td><Link to={`/AwarenessSessions`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
     </tr>
     <tr>
         <td>Cash</td>
-        <td>Male</td>
+        <td><Link to={`/Cash`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
     </tr>
+    <tr>
+        <td>CrowdFunding</td>
+        <td><Link to={`/CrowdFunding`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    <tr>
+        <td>Drives</td>
+        <td><Link to={`/drives`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    <tr>
+        <td>Event Partners</td>
+        <td><Link to={`/EventPartners`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    <tr>
+        <td>Fund Raising</td>
+        <td><Link to={`/FundRaising`}>
+                                    <button className="btn btn-edit">
+                                        View
+                                    </button>
+                                    </Link>
+                                    </td>
+    </tr>
+    
+    
 </table>
   );
 }
